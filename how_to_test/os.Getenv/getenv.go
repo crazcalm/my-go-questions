561fa9f4876098209bdb5d14a9db247a42a8707d
Marcus Willock <crazcalm@gmail.com>
2018-07-28 11:05:57 -0400

    I forgot to commit this a few days ago

--- how_to_test/os.Getenv/getenv.go
+++ how_to_test/os.Getenv/getenv.go
@@ -5,6 +5,18 @@ import (
 	"strings"
 )
 
+func envValue(env, defaultValue string) (result string, err error) {
+	result = os.Getenv(env)
+	if strings.EqualFold(result, "") {
+		err = os.Setenv(env, defaultValue)
+		if err != nil {
+			return
+		}
+		result = os.Getenv(env)
+	}
+	return
+}
+
 func envExist(env string) (result bool) {
 	value := os.Getenv(env)
 	if !strings.EqualFold(value, "") {
--- how_to_test/os.Getenv/getenv_test.go
+++ how_to_test/os.Getenv/getenv_test.go
@@ -10,8 +10,69 @@ package getenv
 
 import (
 	"os"
+	"strings"
 	"testing"
 )
+
+func TestEnvValueExplicit(t *testing.T) {
+	tests := []struct {
+		Key     string
+		Value   string
+		Default string
+		Answer  string
+	}{
+		{"NAME", "", "default1", "default1"},
+		{"NAME", "default1", "default2", "default1"},
+	}
+
+	for i, test := range tests {
+
+		//Need to keep original value so that I can clean up after the test
+		oldValue := os.Getenv(test.Key)
+
+		//Ensuring that the environmental variable exists
+		err := os.Setenv(test.Key, test.Value)
+		if err != nil {
+			t.Fatalf("Failed to set environment variable: %s", err.Error())
+		}
+
+		result, err := envValue(test.Key, test.Default)
+		if err != nil {
+			t.Errorf("Case (%d): envValue returned an err: %s", i, err.Error())
+		}
+
+		if !strings.EqualFold(result, test.Answer) {
+			t.Errorf("Case (%d): Expected %s to be equal to %s, but got %s", i, test.Key, test.Answer, result)
+		}
+
+		err = os.Setenv(test.Key, oldValue)
+		if err != nil {
+			t.Logf("Case (%d): Failed to set environment variable back to %s: %s", i, oldValue, err.Error())
+		}
+	}
+}
+
+func TestEnvValueImplicit(t *testing.T) {
+	tests := []struct {
+		Key     string
+		Default string
+		Answer  string
+	}{
+		{"NAME", "default1", "default1"},
+		{"NAME", "default2", "default1"},
+	}
+
+	for i, test := range tests {
+		result, err := envValue(test.Key, test.Default)
+		if err != nil {
+			t.Errorf("envValue returned an err: %s", err.Error())
+		}
+
+		if !strings.EqualFold(result, test.Answer) {
+			t.Errorf("Case (%d): Expected %s to be equal to %s, but got %s", i, test.Key, test.Answer, result)
+		}
+	}
+}
 
 func TestEnvExist(t *testing.T) {
 	tests := []struct {
